f076bc389487cd72ee52ef4bf5a675253be4622b
towficaziz <tapolly01@gmail.com>
2023-06-11 19:20:36 +0200

    Splitting Components into multiple components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import ExpenseItem from "./components/ExpenseItem";
+import Expenses from "./components/Expenses";
 import "./App.css";
 
 function App() {
@@ -28,32 +28,11 @@ function App() {
       date: new Date(2023, 3, 23),
     },
   ];
-  // const para = document.createElement('p');
-  // para.textContent = 'This is also visible';
-  // document.getElementById('root').append(para);
+
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[3].title}
-        amount={expenses[3].amount}
-        date={expenses[3].date}
-      ></ExpenseItem>
+      <Expenses items={expenses}/>
     </div>
   );
 }
