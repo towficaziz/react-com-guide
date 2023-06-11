127c762d5c4bd67ba52dbf3dbc725994f847de8a
towficaziz <tapolly01@gmail.com>
2023-06-11 14:13:10 +0200

    Use of first props concept

--- src/App.js
+++ src/App.js
@@ -1,14 +1,59 @@
-import ExpenseItem from './components/ExpenseItem';
-import './App.css';
+import ExpenseItem from "./components/ExpenseItem";
+import "./App.css";
 
 function App() {
+  const expenses = [
+    {
+      id: "e1",
+      title: "Toilet Paper",
+      amount: 294.67,
+      date: new Date(2023, 1, 28),
+    },
+    {
+      id: "e2",
+      title: "New TV",
+      amount: 294.67,
+      date: new Date(2023, 2, 21),
+    },
+    {
+      id: "e3",
+      title: "Car Insurance",
+      amount: 214.67,
+      date: new Date(2023, 3, 28),
+    },
+    {
+      id: "e4",
+      title: "Medical",
+      amount: 299.67,
+      date: new Date(2023, 3, 23),
+    },
+  ];
   // const para = document.createElement('p');
   // para.textContent = 'This is also visible';
-  // document.getElementById('root').append(para); 
+  // document.getElementById('root').append(para);
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem></ExpenseItem>
+      <ExpenseItem
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[1].title}
+        amount={expenses[1].amount}
+        date={expenses[1].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[2].title}
+        amount={expenses[2].amount}
+        date={expenses[2].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[3].title}
+        amount={expenses[3].amount}
+        date={expenses[3].date}
+      ></ExpenseItem>
     </div>
   );
 }
