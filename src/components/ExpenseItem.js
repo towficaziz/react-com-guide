d3359f5965252a0b4fc1e2c2aed3f436fac164f3
towficaziz <tapolly01@gmail.com>
2023-06-11 21:33:04 +0200

    Concept of Composition (children props)

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,4 +1,5 @@
 import './ExpenseItem.css';
+import Card from './Card';
 import './ExpenseDate';
 import ExpenseDate from './ExpenseDate';
 
@@ -6,13 +7,13 @@ function ExpenseItem(props) {
     
     
   return (
-    <div className="expense-item">
+    <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className='expense-item__description'>
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
-    </div>
+    </Card>
   );
 }
 
