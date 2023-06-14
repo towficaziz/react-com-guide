d16b67fccca0bbd2657ba01bbefcc59e0c186aea
towficaziz <tapolly01@gmail.com>
2023-06-15 00:29:35 +0200

    using stateful kists

--- src/App.js
+++ src/App.js
@@ -1,40 +1,42 @@
-import React from "react";
+import React, {useState} from "react";
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
 import "./App.css";
 
+const DUMMY_EXPENSES = [
+  {
+    id: "e1",
+    title: "Toilet Paper",
+    amount: 294.67,
+    date: new Date(2023, 1, 28),
+  },
+  {
+    id: "e2",
+    title: "New TV",
+    amount: 294.67,
+    date: new Date(2023, 2, 21),
+  },
+  {
+    id: "e3",
+    title: "Car Insurance",
+    amount: 214.67,
+    date: new Date(2023, 3, 28),
+  },
+  {
+    id: "e4",
+    title: "Medical",
+    amount: 299.67,
+    date: new Date(2023, 3, 23),
+  },
+];
 // function App() {
 const App = () => {
-  const expenses = [
-    {
-      id: "e1",
-      title: "Toilet Paper",
-      amount: 294.67,
-      date: new Date(2023, 1, 28),
-    },
-    {
-      id: "e2",
-      title: "New TV",
-      amount: 294.67,
-      date: new Date(2023, 2, 21),
-    },
-    {
-      id: "e3",
-      title: "Car Insurance",
-      amount: 214.67,
-      date: new Date(2023, 3, 28),
-    },
-    {
-      id: "e4",
-      title: "Medical",
-      amount: 299.67,
-      date: new Date(2023, 3, 23),
-    },
-  ];
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
 
   const addExpenseHandler = expense =>{
-    console.log('In App.js');
-    console.log(expense);
+    setExpenses(prevExpenses =>{
+      return [expense, ...prevExpenses];
+    });
   };
 
   // return React.createElement(
